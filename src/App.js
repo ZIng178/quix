a057b23cd7db457be2a52375dcff7c2dd0f6d166
Wangdi Tenzing <wangdi457@gmail.com>
2021-05-19 21:27:16 -0400

    added background image and a money section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ function App() {
   return (
     <div className="App">
       <div className="main">main</div>
-      <div className="pyramid">money</div>
+      <div className="pyramid">
+        <ul className="moneylist">
+          <li className="moneyListItem active">
+            <span className="moneyListNumber">4</span>
+            <span className="moneyListAmount">$100</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListNumber">4</span>
+            <span className="moneyListAmount">$100</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListNumber">4</span>
+            <span className="moneyListAmount">$100</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListNumber">4</span>
+            <span className="moneyListAmount">$100</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
